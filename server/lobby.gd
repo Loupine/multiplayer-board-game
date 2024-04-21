5656e17d6fd3d98a496c147af78374e0d3a7b22f
Andrew Everage-Scheible <andreweveragescheible@gmail.com>
2024-04-21 01:46:41 -0400

    Add server logic to handle disconnection and reconnection after the game starts

--- server/lobby.gd
+++ server/lobby.gd
@@ -13,17 +13,10 @@ const MAX_CONNECTIONS = 20
 
 # This will contain player info for every player,
 # with the keys being each player's unique IDs.
-var players = {}
-
-# This is the local player info. This should be modified locally
-# before the connection is made. It will be passed to every other peer.
-# For example, the value of "name" can be set to something the player
-# entered in a UI scene.
-var player_info = {
-	"name": "Name",
-	"board_position": "Position"
-}
-var players_loaded = 0
+var players : Dictionary = {}
+var disconnected_players = {}
+var players_loaded : int = 0
+var game_started := false
 
 
 func _ready():
@@ -60,20 +53,56 @@ func player_loaded():
 
 # When server receives new player info from a client, send that info to all other players
 @rpc("any_peer", "call_remote", "reliable")
-func _server_receive_player_info(new_player_info)-> void:
+func _server_receive_player_info(new_player_info: Dictionary)-> void:
+	print(game_started)
 	var sender_id = multiplayer.get_remote_sender_id()
 	if players.has(sender_id):
 		pass
 	else:
-		_send_new_player_info_to_players(sender_id, new_player_info)
-		players[sender_id] = new_player_info
+		if game_started:
+			if !disconnected_players.is_empty():
+				print("Trying player reconnect")
+				_try_player_reconnect(sender_id, new_player_info)
+			else:
+				print("Game started and no players are disconnected")
+				multiplayer.multiplayer_peer.disconnect_peer(sender_id)
+		elif multiplayer.get_peers().size() == MAX_CONNECTIONS:
+			multiplayer.multiplayer_peer.disconnect_peer(sender_id)
+		else:
+			_send_new_player_info_to_players(sender_id, new_player_info)
+			players[sender_id] = new_player_info
+
+
+func _try_player_reconnect(id, info)->void:
+	var player_reconnected := false
+	for player in players:
+		var connected_player_info = players.get(player)
+		if connected_player_info["name"] == info["name"]:
+			print("This player is already connected!")
+			multiplayer.multiplayer_peer.disconnect_peer(id, true)
+			return
+	for player in disconnected_players:
+		var disconnected_player_info = disconnected_players.get(player)
+		if disconnected_player_info["name"] == info["name"]:
+			info["board_position"] = disconnected_player_info["board_position"]
+			_send_new_player_info_to_players(id, info)
+			players[id] = info
+			disconnected_players.erase(player)
+			player_reconnected = true
+			print("Player successfully reconnected")
+			break
+	if !player_reconnected:
+		multiplayer.multiplayer_peer.disconnect_peer(id, true)
 
 
 # Server method, called above, that sends new player info to other players
 func _send_new_player_info_to_players(new_player_id, info)->void:
 	for player in players:
+		print(players.get(player)["name"])
 		_register_player.rpc_id(player, new_player_id, info)
 
+		var existing_player_info = players.get(player)
+		_register_player.rpc_id(new_player_id, player, existing_player_info)
 
 # Client method that adds new players with info sent from server
 @rpc("authority", "call_remote", "reliable")
@@ -87,6 +116,10 @@ func _on_player_connected(id):
 
 func _on_player_disconnected(id):
 	print("Player %d, disconnected!" % id)
+	if game_started and !players.is_empty():
+		disconnected_players[id] = players.get(id)
+		disconnected_players.get(id)["connection_status"] = "Disconnected"
+	players.erase(id)
 
 
 func _on_connected_fail():

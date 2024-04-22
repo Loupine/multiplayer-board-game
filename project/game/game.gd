592fca3d89c3e80c00d6c4182c67231a8f7afc96
Andrew Everage-Scheible <andreweveragescheible@gmail.com>
2024-04-22 01:29:06 -0400

    Add initial player spawn and multiplayer synchronization

--- project/game/game.gd
+++ project/game/game.gd
@@ -1,8 +1,34 @@
 extends Node2D
 
+var turn_order :Array
+
 
 func _ready():
 	Lobby.player_loaded.rpc_id(1)
+	%PlayerSpawner.add_spawnable_scene("res://player/player.tscn")
+
+
+func spawn_player()->void:
+	pass
+
+
+# The server sends the generated turn order to clients
+@rpc("authority", "call_remote", "reliable")
+func _send_turn_order(order: Array)->void:
+	turn_order = order
+
+
+# The server starts the next player's turn
+@rpc("authority", "call_remote", "reliable")
+func _start_player_turn(player_id)->void:
+	if multiplayer.get_unique_id() == player_id:
+		_start_turn()
+	else:
+		print("%s's turn started." % Lobby.players.get(player_id)["name"])
+
+
+func _start_turn():
+	print("Turn started")
 
 
 # Client should call this when certain actions finish during their turn
--- server/game/game.gd
+++ server/game/game.gd
@@ -5,18 +5,56 @@ signal turn_finsihed()
 signal round_finished(number)
 signal game_finished()
 
-var turn_order : Array[String] = []
+var turn_order : Array = []
+var current_turn_index := 0
 var round_number := 1
 var players = Lobby.players
 var random_generator = RandomNumberGenerator.new()
 
 
 func _ready():
+	%PlayerSpawner.add_spawnable_scene("res://player/player.tscn")
 	random_generator.randomize()
 
 
 func start_game()->void:
+	spawn_players()
 	_determine_player_turn_order()
+	_send_turn_order.rpc(turn_order)
+	_start_player_turn.rpc(turn_order[current_turn_index])
+
+
+func spawn_players()->void:
+	for player in players:
+		var player_body := create_player(player)
+		%Players.add_child(player_body)
+		player_body.set_multiplayer_authority(player)
+
+
+func create_player(player_id)->CharacterBody2D:
+	var player_body := preload("res://player/player.tscn").instantiate()
+	player_body.name = str(player_id)
+	player_body.position = %BoardPositions.get_child(0).position + (
+							Vector2(random_generator.randi_range(-100, 100),
+									random_generator.randi_range(-100, 100)))
+	var modulate_color := Color(random_generator.randi_range(1, 255),
+								random_generator.randi_range(1, 255),
+								random_generator.randi_range(1, 255),
+								255)
+	player_body.set_modulate(modulate_color)
+	return player_body
+
+
+# The server sends the generated turn order to clients
+@rpc("authority", "call_remote", "reliable")
+func _send_turn_order(_order)->void:
+	pass
+
+
+# The server starts the next player's turn
+@rpc("authority", "call_remote", "reliable")
+func _start_player_turn(_player_id)->void:
+	pass
 
 
 # Client should call this when certain actions finish during their turn

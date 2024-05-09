1bb279babab354cec6e4cd5d458ec288e5a1ec37
Andrew Everage-Scheible <andreweveragescheible@gmail.com>
2024-05-09 03:30:49 -0400

    Rename PlayerControls to ControlsUI

--- project/game/actions/actions.gd
+++ project/game/actions/actions.gd
@@ -4,8 +4,8 @@ extends Node
 var _move_action: MoveAction
 
 
-func _init(scene_tree: SceneTree, multiplayer_id: int, total_board_positions: int, board_positions_node: Node, player_controls_node: Node)->void:
-	_move_action = MoveAction.new(scene_tree, multiplayer_id, total_board_positions, board_positions_node, player_controls_node)
+func _init(scene_tree: SceneTree, multiplayer_id: int, total_board_positions: int, board_positions_node: Node, controls_node: Node)->void:
+	_move_action = MoveAction.new(scene_tree, multiplayer_id, total_board_positions, board_positions_node, controls_node)
 
 
 func update_current_player_node(current_player_node: Node)->void:
